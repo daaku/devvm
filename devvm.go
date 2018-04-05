c83bd9e5b0ceb67d1a4f12f95ece72bba770fe64
Naitik Shah <n@daaku.org>
2018-04-05 14:40:54 +0400

    add some resilience and port checking

--- devvm.go
+++ devvm.go
@@ -6,6 +6,8 @@ package main // import "github.com/daaku/devvm"
 import (
 	"flag"
 	"fmt"
+	"net"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -14,6 +16,7 @@ import (
 
 func main() {
 	instanceID := flag.String("instance", "", "instance id")
+	port := flag.Int("port", 22, "port to wait for")
 	flag.Parse()
 	session, err := session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
@@ -28,11 +31,28 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	out, err := svc.DescribeInstances(&ec2.DescribeInstancesInput{
-		InstanceIds: aws.StringSlice([]string{*instanceID}),
-	})
-	if err != nil {
-		panic(err)
+	var dnsName string
+	for {
+		out, err := svc.DescribeInstances(&ec2.DescribeInstancesInput{
+			InstanceIds: aws.StringSlice([]string{*instanceID}),
+		})
+		if err != nil {
+			panic(err)
+		}
+		dnsName = *out.Reservations[0].Instances[0].PublicDnsName
+		if dnsName == "" {
+			time.Sleep(10 * time.Millisecond)
+			continue
+		}
+		fmt.Println(dnsName)
+		break
 	}
-	fmt.Println(*out.Reservations[0].Instances[0].PublicDnsName)
+	for {
+		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", dnsName, *port))
+		if err != nil {
+			continue
+		}
+		conn.Close()
+		break
+	}
 }
